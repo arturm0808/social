3c4c1964bd1a11c515c0eb031b7618dba4ab1f12
Artur Maizelev <arturm@securitydam.com>
2020-12-15 16:27:50 +0200

    Add delete post 😈

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server");
+
 const Post = require("../../models/post");
 const checkAuth = require("../../util/checkAuth");
 
@@ -5,7 +7,7 @@ module.exports = {
   Query: {
     getPosts: async () => {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (err) {
         throw new Error(err);
@@ -27,7 +29,7 @@ module.exports = {
   Mutation: {
     createPost: async (_, { body }, context) => {
       const user = checkAuth(context);
-      console.log(user);
+
       const newPost = new Post({
         body,
         user: user.id,
@@ -37,6 +39,23 @@ module.exports = {
 
       const post = await newPost.save();
       return post;
+    },
+    deletePost: async (_, { postId }, context) => {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+
+        if (user.username === post.username) {
+          await post.delete();
+
+          return "Post Deleted ✔";
+        } else {
+          throw new AuthenticationError("Action not allowed 🤚");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 };
--- util/checkAuth.js
+++ util/checkAuth.js
@@ -7,6 +7,7 @@ module.exports = (context) => {
   const authHeader = context.req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split("Bearer ")[1];
+
     if (token) {
       try {
         const user = jwt.verify(token, SECRET_KEY);
