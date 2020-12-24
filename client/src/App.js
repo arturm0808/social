ec7641a1b4d495b957f334b8ceafdb38edbec39d
Artur Maizelev <arturm@securitydam.com>
2020-12-24 17:10:13 +0200

    Add routes 🍾

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import MenuBar from "./components/MenuBar/MenuBar";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
 const App = () => {
-  return <h1>💩</h1>;
+  return (
+    <Router>
+      <Container>
+        <MenuBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </Container>
+    </Router>
+  );
 };
 
 export default App;
