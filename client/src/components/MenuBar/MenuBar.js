376b881dc6d9dee9d9c2fa3ebaddc88767527789
Artur Maizelev <arturm@securitydam.com>
2020-12-24 17:30:28 +0200

    Add routes 🍾

---
+++ client/src/components/MenuBar/MenuBar.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+
+const MenuBar = () => {
+  const [activeItem, setActiveItem] = useState("");
+  const { pathname } = useLocation();
+
+  // pathname.split("/")[1]
+
+  useEffect(() => {
+    if (pathname === "/") {
+      setActiveItem("home");
+    } else {
+      setActiveItem(pathname.substr(1));
+    }
+  }, [pathname]);
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+  };
+
+  return (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item
+        name="home"
+        active={activeItem === "home"}
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
+      <Menu.Menu position="right">
+        <Menu.Item
+          name="login"
+          active={activeItem === "login"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/login"
+        />
+        <Menu.Item
+          name="register"
+          active={activeItem === "register"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/register"
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+};
+
+export default MenuBar;
