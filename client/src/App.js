a5cba233c4fd577f062544e85360e8aa771688f7
Artur Maizelev <arturm@securitydam.com>
2020-12-31 18:17:38 +0200

    Add authRoute 😈

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import MenuBar from "./components/MenuBar/MenuBar";
+import AuthRoute from "./util/AuthRoute";
+
 import { AuthProvider } from "./context/auth";
 
 import "semantic-ui-css/semantic.min.css";
@@ -18,8 +20,8 @@ const App = () => {
           <MenuBar />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
+            <AuthRoute exact path="/login" component={Login} />
+            <AuthRoute exact path="/register" component={Register} />
           </Switch>
         </Container>
       </Router>
